7c500607b1536373ee9c9e92753ace6249503962
Roelan <100556622+dakkkkkk@users.noreply.github.com>
2024-05-23 22:35:24 +0800

    Update appliedAi.py

--- appliedAi.py
+++ appliedAi.py
@@ -1,36 +1,50 @@
 import streamlit as st
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+import language_tool_python
 
 st.title("Text Summarizer with Grammar Checker")
-
-
 
 st.header("Enter Text")
 input_text = st.text_area("Paste your text here:", height=200)
 
-
 if st.button("Summarize Text"):
-    st.write("Summarized text will appear here.")
- 
-    summarized_text = "This is where the summarized text will be displayed."
-    st.success(summarized_text)
+    if input_text:
+        parser = PlaintextParser.from_string(input_text, Tokenizer("english"))
+        summarizer = LsaSummarizer()
+        summary = summarizer(parser.document, sentences_count=2)
+        summarized_text = " ".join([str(sentence) for sentence in summary])
+        st.success(summarized_text)
+    else:
+        st.warning("Please enter text to summarize.")
 
 if st.button("Check Grammar"):
-    st.write("Corrected text will appear here.")
-   
-    corrected_text = "This is where the grammar-corrected text will be displayed."
-    st.success(corrected_text)
-
+    if input_text:
+        tool = language_tool_python.LanguageTool('en-US')
+        matches = tool.check(input_text)
+        corrected_text = language_tool_python.utils.correct(input_text, matches)
+        st.success(corrected_text)
+    else:
+        st.warning("Please enter text to check grammar.")
 
 st.header("Output")
 st.write("Here you will see the summarized and/or grammar-checked text based on your actions above.")
 
-
 if st.checkbox("Show Summarized and Corrected Text"):
-    st.write("Combined summarized and corrected text will appear here.")
-    combined_text = "This is where the combined text will be displayed."
-    st.info(combined_text)
+    if input_text:
+        parser = PlaintextParser.from_string(input_text, Tokenizer("english"))
+        summarizer = LsaSummarizer()
+        summary = summarizer(parser.document, sentences_count=2)
+        summarized_text = " ".join([str(sentence) for sentence in summary])
+        
+        tool = language_tool_python.LanguageTool('en-US')
+        matches = tool.check(summarized_text)
+        combined_text = language_tool_python.utils.correct(summarized_text, matches)
+        
+        st.info(combined_text)
+    else:
+        st.warning("Please enter text to summarize and check grammar.")
 
 # Footer
 st.write("This application provides both text summarization and grammar checking functionalities. Enter your text above to get started.")
-
-
